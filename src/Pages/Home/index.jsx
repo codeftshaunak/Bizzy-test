843dfeedbfc8af5be36bd4393aa8f35a2971ea31
codeftshaunak <sagardeyyash19@gmail.com>
2023-09-22 14:29:32 +0600

    update component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,12 +3,17 @@ import HomeLayout from '../../Layouts/HomeLayout';
 import { Link } from 'react-router-dom';
 import { Button, HStack } from '@chakra-ui/react';
 import HomeComp from './HomeComp';
+import { HomeFooter } from '../../Components/Footer';
 
 const Home = () => {
     return (
-        <HomeLayout>
-            <HomeComp />
-        </HomeLayout>
+        <>
+            <HomeLayout>
+                <HomeComp />
+            </HomeLayout>
+            <HomeFooter />
+        </>
+
     )
 }
 
